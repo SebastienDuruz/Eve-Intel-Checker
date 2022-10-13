fd9c5931e4ac079e4a71b579f58e41ca2ca28ed7
Seb-Dz <sebastienduruz@protonmail.com>
2022-10-13 22:56:06 +0200

    Adding UserSettings Json File Reader

---
+++ EveIntelChecker/EveIntelCheckerLib/Data/UserSettingsReader.cs
@@ -0,0 +1,62 @@
+﻿/// Author : Sébastien Duruz
+/// Date : 13.10.2022
+
+using EveIntelCheckerLib.Models;
+using Newtonsoft.Json;
+using System;
+using System.IO;
+
+namespace EveIntelCheckerLib.Data
+{
+    /// <summary>
+    /// Class UserSettingsReader
+    /// </summary>
+    public class UserSettingsReader
+    {
+        /// <summary>
+        /// File path of the userSettings file
+        /// </summary>
+        private string _filePath = "userSettings.json";
+
+        /// <summary>
+        /// Objects that contains the settings values
+        /// </summary>
+        public UserSettings UserSettingsValues { get; set; }
+
+        public UserSettingsReader()
+        {
+            ReadUserSettings();
+        }
+
+        /// <summary>
+        /// Read the userSettings json file, create it if not exists
+        /// </summary>
+        public void ReadUserSettings()
+        {
+            if(File.Exists(_filePath))
+            {
+                try
+                {
+                    UserSettingsValues = JsonConvert.DeserializeObject<UserSettings>(File.ReadAllText(_filePath));
+                }
+                catch (Exception ex)
+                {
+                    // TODO : Do somethng with exception triggered
+                }
+            }
+            else
+            {
+                UserSettingsValues = new UserSettings();
+                WriteUserSettings();
+            }
+        }
+
+        /// <summary>
+        /// Write the UserSettings object to json file
+        /// </summary>
+        public void WriteUserSettings()
+        {
+            File.WriteAllText(_filePath, JsonConvert.SerializeObject(UserSettingsValues));
+        }
+    }
+}
---
+++ EveIntelChecker/EveIntelCheckerLib/Models/UserSettings.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EveIntelCheckerLib.Models
+{
+    /// <summary>
+    /// Class UserSettings
+    /// </summary>
+    public class UserSettings
+    {
+        /// <summary>
+        /// Last file opened by the application
+        /// </summary>
+        public string LastFileName { get; set; } = "";
+    }
+}
